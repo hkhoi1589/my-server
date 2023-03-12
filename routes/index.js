081331857a2f362c5f2ca45ac993bcfa832f317f
hkhoi1589 <hkhoi1589@gmail.com>
2023-03-12 22:00:33 +0700

    fix

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const { verifyKey, isEmpty } = require('../helpers');
 
 // kiem tra token tu cac request den server
 const verifyToken = (req, res, next) => {
-	console.log(req.headers);
 	const token = req.body.token || req.query.token || req.headers['authorization'];
 	if (!token) {
 		return res.status(403).send('A token is required for authentication');
